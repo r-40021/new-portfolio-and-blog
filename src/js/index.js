56f0d29dc90b4204301e85818003d82757f6097a
Frog <frog@frogapp.net>
2023-04-03 11:46:31 +0900

    Fix error

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
 import { faGlobe, faArrowLeft, faArrowRight, faAngleDoubleLeft, faAngleDoubleRight, faEllipsis, faUpRightFromSquare, faSun, faMoon, faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons'
 // 各種 JS 読み込み
+/* eslint-disable */
 import { Dropdown } from 'bootstrap/js/dist/dropdown'
 import { Collapse } from 'bootstrap/js/dist/collapse'
+/* eslint-disable */
 import './clipboard'
 import './flexsearch'
 import './link'
@@ -15,6 +17,3 @@ import './tooltip'
 
 library.add(faGlobe, faArrowLeft, faArrowRight, faAngleDoubleLeft, faAngleDoubleRight, faEllipsis, faUpRightFromSquare, faSun, faMoon, faCircleHalfStroke)
 dom.i2svg()
-
-Dropdown()
-Collapse()
